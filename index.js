9ea22313f1c8646534d6f105fa803aebe34bbf44
Riya Rashmi <56516797+riya-star@users.noreply.github.com>
2020-05-17 22:20:00 +0530

    Drum kit
    This is my first javascript based mini project , where I learnt how to apply javascript on our website and make it more responsive
    From taking inputs from the keyboard and mouse to adding animation , I've learnt and applied my knowledge here ,
    Will work hard and soon make a project on a greater scale with better performance :D
    #100DayOfCode

---
+++ index.js
@@ -0,0 +1,57 @@
+
+// detecting click
+for(var i=0;i<document.querySelectorAll(".drum").length;i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+
+   var keyPressed=this.innerHTML;
+   makeSound(keyPressed);
+   highlight(keyPressed);
+
+  });
+}
+
+// detecting keyboard
+document.addEventListener("keydown",function(event){
+  var keyclicked=event.key;
+  makeSound(keyclicked);
+  highlight(keyclicked);
+});
+
+// adding music
+function makeSound(key){
+  switch (key) {
+                      case 'w':var crash=new Audio("sounds/crash.mp3");
+                              crash.play();
+                              break;
+                      case 'a':var kick=new Audio("sounds/kick-bass.mp3");
+                               kick.play();
+                               break;
+                     case 's':var snare=new Audio("sounds/snare.mp3");
+                             snare.play();
+                             break;
+                     case 'd':var tom1=new Audio("sounds/tom-1.mp3");
+                              tom1.play();
+                              break;
+                      case 'j':var tom2=new Audio("sounds/tom-2.mp3");
+                              tom2.play();
+                              break;
+                      case 'k':var tom3=new Audio("sounds/tom-3.mp3");
+                               tom3.play();
+                               break;
+                      case 'l':var tom4=new Audio("sounds/tom-4.mp3");
+                               tom4.play();
+                               break;
+
+    default: console.log(key);
+  }
+};
+
+// making flash
+function highlight(button){
+  var active=document.querySelector("."+button);
+  active.classList.add("pressed");
+  setTimeout(function(){
+    active.classList.remove("pressed"),100
+  });
+
+};
